e59f5495f98992ead73af0430ab11003ca97f204
KrapivinAV <min_art@mail.ru>
2020-10-04 19:45:47 +0300

    Добавляет заготовку для маршрутизации

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main";
-
+import LoginScreen from "../login-screen/login-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import PropertiesScreen from "../properties-screen/properties-screen";
 
 const App = (props) => {
   const {offersCount} = props;
 
   return (
-    <Main offersCount={offersCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main offersCount={offersCount} />
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/offer/:id?">
+          <PropertiesScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
