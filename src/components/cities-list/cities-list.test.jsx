e025e0add27452b19112c318171d3023ce4f6f75
KrapivinAV <min_art@mail.ru>
2020-11-30 11:37:32 +0300

    WIP

---
+++ src/components/cities-list/cities-list.test.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import CitiesList from "./cities-list";
+
+const CURRENT_CITY = `Paris`;
+
+describe(`Render CitiesList`, () => {
+  it(`Render CitiesList`, () => {
+    const tree = renderer
+      .create(
+          <CitiesList
+            currentCity={CURRENT_CITY}
+            onCityLinkClick={() => {}}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
